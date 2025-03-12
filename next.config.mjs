35db5f2fd55b3035159572373d66c04f7e4df7d5
Mandad Tabrizi <mandad.tabrizi@gmail.com>
2025-03-12 07:34:48 -0700

    fix: simplify config and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, must-revalidate'
-          }
-        ],
-      },
-    ]
-  }
+  swcMinify: true
 }
 
 export default nextConfig; 
