93d0fc33958acffbcc105c3f413f9cd57efeb503
Mandad Tabrizi <mandad.tabrizi@gmail.com>
2025-03-12 07:26:21 -0700

    chore: add vercel config and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    appDir: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate'
+          }
+        ],
+      },
+    ]
+  }
 }
 
-export default nextConfig 
+export default nextConfig; 
